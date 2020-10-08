41b4894cf5bdf27e35763758e981d06269cef163
JeanPierreLeDouche <38559253+JeanPierreLeDouche@users.noreply.github.com>
2020-10-08 11:50:22 +0200

    Intialized a file with some stuff in it

---
+++ GlobeModel.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 11:47:29 2020
+
+@author: Gebruiker
+"""
+
+### Impressive arithmatic
+
+a = 2
+b = 3 
+c = a * b 
+
+### cutting edge modelling: 
+
+def tempfunc(insolation, CO2_conc, mass_ice, c_1, c_2, c_3):
+    T = c_1 * insolation + c_2 * CO2_conc + c_3 * mass_ice
+    return T
+
